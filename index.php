75ed5ad94c4a421d37253079be363108c1079385
songhoang2000hn <66767531+songhoang2000hn@users.noreply.github.com>
2020-06-16 16:10:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
 <?php
     echo "I love You!";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+
+<body>
+    <p>mlem mlem :p</p>
+
+
+</body>
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        background-color: #ffffff;
+        color: #5a5a5a;
+        font-family: "Lato", sans-serif;
+        font-size: 20px;
+        text-rendering: optimizeLegibility;
+    }
+
+    html {
+        scroll-behavior: smooth;
+    }
+
+    body {
+        overflow-x: hidden;
+    }
+</style>
+
+</html>
