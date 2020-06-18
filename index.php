464a13afcacc5ca4c2a22ec3ccc36dfe868619d8
songhoang2000hn <66767531+songhoang2000hn@users.noreply.github.com>
2020-06-18 08:29:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,14 @@
-<?php
-    echo "I love You!";
-?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta charset="UTF-8">
+	<title>Login Page</title>
 </head>
-
-<body>
-    <p>mlem mlem :p</p>
-
-
+<body >
+<<form action="Connect.php" method="POST" accept-charset="utf-8">
+Username:<input type="text" name="username" value="" placeholder="username"><br/>
+Password:<input type="password" name="pass" value="" placeholder="**********"><br/>
+<input type="submit" name="" value="Login"><br/>
+</form>
 </body>
-<style>
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-
-    html {
-        background-color: #ffffff;
-        color: #5a5a5a;
-        font-family: "Lato", sans-serif;
-        font-size: 20px;
-        text-rendering: optimizeLegibility;
-    }
-
-    html {
-        scroll-behavior: smooth;
-    }
-
-    body {
-        overflow-x: hidden;
-    }
-</style>
-
 </html>
